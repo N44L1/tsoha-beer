43cf191ff075d5ddb6e3388c107463712c7479a4
N44L1 <otso.vaisanen@netcourrier.com>
2024-04-14 11:48:13 +0300

    Added flask_bootstrap to help with look of site

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from os import getenv
 
 # Main flask application file
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = getenv("SECRET_KEY")
 
 import beer
